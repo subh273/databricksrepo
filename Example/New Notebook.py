566f66e4b47b053c9fb38742f6e556639e7e11a3
subh273 <subh273>
2023-03-16 04:16:17 +0000

    new changes

--- Example/New Notebook.py
+++ Example/New Notebook.py
@@ -1,2 +1,14 @@
 # Databricks notebook source
+# MAGIC %sql
+# MAGIC CREATE OR REPLACE TEMP VIEW temp_vw_sumit(id, name, value) AS VALUES
+# MAGIC (1, "A", 1.0),
+# MAGIC (2, "B", 2.1)
 
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM temp_vw_sumit
+
+# COMMAND ----------
+
+display(spark.sql("SELECT * FROM temp_vw_sumit"))
